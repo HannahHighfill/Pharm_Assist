fe2369909a281d859fe6c180470b5dd3af3ff65e
Jamie Git Git <justicehopelove@yahoo.com>
2019-03-15 14:54:52 -0700

    added example calendar api to calendar view

--- microblog/views.py
+++ microblog/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth import logout as auth_logout
 
 from .models import Tweet
 
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
 
 class NewUserForm(forms.ModelForm):
@@ -70,6 +71,44 @@ def logout(request):
     return redirect(request.META.get('HTTP_REFERER', '/'))
     
 def calendar(request):
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+        
     context={
         'username':request.user.username 
     }
