1664edeccac832167d266e2cd358bdc81735b807
Jamie Git Git <justicehopelove@yahoo.com>
2019-03-23 12:21:35 -0700

    deleted extraneous model

--- microblog/models.py
+++ microblog/models.py
@@ -38,21 +38,3 @@ class Refill(models.Model):
     repeats = models.CharField(max_length=9, choices= REPEATS_CHOICES, null=True, blank=True, verbose_name="")
 
 
-#Jamie thinks we will end up deleting this
-class RefillEvent(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
-    
-    # Added Refill form fields from views.py: 'Prescription', 'Nickname', 'Pharmacy', 'Date'
-    prescription = models.CharField(max_length=160)
-    nickname = models.CharField(max_length=160)
-    pharmacy = models.TextField()
-    number = models.CharField(max_length=160)
-    timezone = models.CharField(max_length=160, default='America/Los_Angeles')
-    postedtocalendar = models.BooleanField(default=False)
-#    recurrence = 'RRULE:FREQ=DAILY;COUNT=2'
-#    reminder = models.BooleanField()
-# This will create a table in sqlite. make a form from it. use the form to populate an event variable. the calendar views adds that event to the calendar
-
