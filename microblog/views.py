68e0695eade4f9aea7d0dd6cc7ef0b1abdf4252c
Hannah Highfill <hannahhighfill@gmail.com>
2019-03-23 13:14:41 -0700

    new med curve update

--- microblog/views.py
+++ microblog/views.py
@@ -21,7 +21,6 @@ from google.oauth2 import credentials
 from google.oauth2.credentials import Credentials
 import httplib2
 from django.contrib.auth import authenticate
-from secret_settings import *
 
 from .models import Refill
 
